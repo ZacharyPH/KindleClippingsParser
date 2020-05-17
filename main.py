c7da41c9d1e0afe8a7c902d1fc16a77d59fa63d1
Zachary Hunt <=>
2020-05-17 02:45:40 -0500

    data cleanup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    filename = "My Clippings 1.txt"
+    filename = "My Clippings.txt"
     extract(filename)
 
 
@@ -30,16 +30,14 @@ def parse(chunk):
     return {"Book": book, "Author": author, "Date": date, "Location": loc, "Clip": clip}
 
 
-def write(data, filename="out1.csv"):
-    # TODO: Filter duplicates
+def write(data, filename="out.csv"):
+    # TODO: Filter duplicates?
     out = open("./data/" + filename, "w", encoding="UTF8")
     items = ["Book", "Author", "Date", "Location", "Clip"]
     out.write(",".join(items) + "\n")
-    for clip in data:   # TODO: Cleanup .split / .join pairs with .replace for escaping commas
-        msg = clip["Clip"].replace(",", "\\,").replace("\"", "\'")
-        if len(msg.strip(" ")) == 0:    # Empty clip
-            continue    # TODO: Now obsolete, I think
-        out.write(",".join(clip[item].replace(",", "\\,") for item in items[:-1]) + "," + msg + "\n")
+    for clip in data:
+        msg = clip["Clip"].replace("\"", "\'").replace(",", ";").replace("\n", "\\n")
+        out.write(",".join(clip[item].replace(",", ";") for item in items[:-1]) + "," + msg + "\n")
     out.close()
 
 
