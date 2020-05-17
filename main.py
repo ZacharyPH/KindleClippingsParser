51e695c8e82d68829b904ca5b342c09b6e8693ca
Zachary Hunt <=>
2020-05-17 04:05:29 -0500

    duplicate checking, better sorting, writing to output

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     filename = "My Clippings.txt"
     data = extract(filename)
-    write(data)
+    # Can sort by Book, Author, or None. Date, Location, and Clip are unpredictable
+    write(data, sort_by="Book", remove_duplicates=True)
 
 
 def extract(file: str) -> list:
@@ -10,7 +11,7 @@ def extract(file: str) -> list:
     :param file: Clippings Source
     :return: list of dictionaries of clip data
     """
-    # TODO: Implement sorting by book or time
+    # TODO: Extract context from book - this will probably be difficult :)
     clips = open("./Data/" + file, "r", encoding="UTF8")
     chunk = []  # Information for one clip
     data = []   # Collected clips data
@@ -33,31 +34,67 @@ def parse(chunk: list) -> dict:
     book_and_auth, loc_and_date, _, *clip = (item.strip(" \n") for item in chunk)
     if len(clip := "\n".join(clip).strip(" \n")) == 0:  # Empty Clips
         return {}
-    *loc, date = loc_and_date.split(" | ")
-    loc = " | ".join([l.strip("- Highlight Bookmark Loc.") for l in loc])
+    *location, date = loc_and_date.split(" | ")
+    location = " | ".join([loc.strip("- Highlight Bookmark Loc.") for loc in location])
     date = date.strip("Added on")
     # The below logic handles clips from books with no noted author
     book, author = (l[0].strip(" "), l[-1].strip(")")) if len(l := book_and_auth.split("(")) >= 2 else (l[0], "")
-    return {"Book": book, "Author": author, "Date": date, "Location": loc, "Clip": clip}
+    return {"Book": book, "Author": author, "Date": date, "Location": location, "Clip": clip}
 
 
-def write(data: list, filename: str = "out.csv") -> bool:
+def sort_clips(data: list, param="Book") -> list:
+    """
+    Sorting first by dates would be a little bit more complicated,
+    but Python sorts are guaranteed to be stable, so as long as the data was originally ordered
+    (which it is, coming from the Kindle text file), we don't have to worry about it!
+    :param param: field by which to sort the clips. None results in an unchanged list
+    :param data: list of clip data dictionaries
+    :return: sorted list of clip data dictionaries
+    """
+    if param is None:
+        return data
+    return sorted(data, key=lambda d: d[param])
+
+
+def write(data: list, filename: str = "out.csv", sort_by: str = "Book", remove_duplicates: bool = True) -> None:
     """
     Writes results to file
+    :param sort_by:
+    :param remove_duplicates: filter out duplicates? Takes a little longer and ASSUMES A SORTED LIST
     :param data: list of clip data dictionaries
     :param filename: location of output
     :return: True if successful
     """
-    # TODO: Filter duplicates?
+    if remove_duplicates:
+        data = sort_clips(data, param="Book")
     out = open("./data/" + filename, "w", encoding="UTF8")
     items = ["Book", "Author", "Date", "Location", "Clip"]
     out.write(",".join(items) + "\n")   # Writing header
-    for clip in data:   # Writing each clip
-        # Replacing (double quotes -> single quotes), (commas -> semicolons), (newlines -> '\n')
-        msg = clip["Clip"].replace("\"", "\'").replace(",", ";").replace("\n", "\\n")
-        out.write(",".join(clip[item].replace(",", ";") for item in items[:-1]) + "," + msg + "\n")
+    if remove_duplicates:
+        prev = "Lorem Ipsum"
+        skips = []
+        for i, clip in enumerate(data):  # Writing each clip
+            # Replacing (double quotes -> single quotes), (commas -> semicolons), (newlines -> '\n')
+            # This is bad practice, modifying data in place. Save space, and I'm being careful (I think :))
+            clip["Clip"] = clip["Clip"].replace("\"", "\'").replace(",", ";").replace("\n", "\\n")
+            if clip["Clip"] in prev:
+                skips.append(i)
+            elif prev in clip["Clip"]:
+                skips.append(i - 1)
+            else:
+                prev = clip["Clip"]
+        for clip in sort_clips([clip for i, clip in enumerate(data) if i not in skips], param=sort_by):
+            line = ",".join(clip[item].replace(",", ";") for item in items) + "\n"
+            out.write(line)
+
+    else:
+        for clip in data:   # Writing each clip
+            # Replacing (double quotes -> single quotes), (commas -> semicolons), (newlines -> '\n')
+            msg = clip["Clip"].replace("\"", "\'").replace(",", ";").replace("\n", "\\n")
+            line = ",".join(clip[item].replace(",", ";") for item in items[:-1]) + "," + msg + "\n"
+            out.write(line)
+
     out.close()
-    return True
 
 
 if __name__ == "__main__":
